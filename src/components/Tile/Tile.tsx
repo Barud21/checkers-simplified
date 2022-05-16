8643f7ee459b830d67db8c4c960d299ee07a25ee
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-05-16 22:16:39 +0200

    Using props to get properties using classNames to add class name if property is true

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,14 +1,13 @@
+import classNames from "classnames";
 import "./Tile.css";
 
-interface Props {
-  number: number;
-}
-
-function Tile({ number }: Props) {
-  if (number % 2 === 0) {
-    return <div className="tile black-tile"></div>;
+function Tile(props: { number: number; isTherePiece: boolean }) {
+  if (props.number % 2 !== 0) {
+    return <div className={"tile white-tile"}></div>;
   } else {
-    return <div className="tile white-tile"></div>;
+    return (
+      <div className={classNames({ piece: props.isTherePiece === true })}></div>
+    );
   }
 }
 
