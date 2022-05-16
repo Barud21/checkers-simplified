c983106838930193b0630ffedd8b84eb276913d2
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-05-16 22:20:41 +0200

    Added default piece position, passing piece position to Tile

--- src/components/Checkerboard/Checkerboard.tsx
+++ src/components/Checkerboard/Checkerboard.tsx
@@ -1,19 +1,16 @@
 import "./Checkerboard.css";
 import Tile from "../Tile/Tile";
 
-const horizontalAxis: Array<string> = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-];
+// prettier-ignore
+const horizontalAxis: Array<string> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",];
 const verticalAxis: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+interface Piece {
+  x: number;
+  y: number;
+}
+
+const piece: Piece[] = [{ x: 4, y: 4 }];
 
 function Board() {
   let board = [];
@@ -21,8 +18,15 @@ function Board() {
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
       const number = j + i + 2;
+      let isTherePiece: boolean = false;
 
-      board.push(<Tile number={number} />);
+      piece.forEach((p) => {
+        if (p.x === i && p.y === j) {
+          isTherePiece = true;
+        }
+      });
+
+      board.push(<Tile number={number} isTherePiece={isTherePiece} />);
     }
   }
   return <div id="checkerboard">{board}</div>;
