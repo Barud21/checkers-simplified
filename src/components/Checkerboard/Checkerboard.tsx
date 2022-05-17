6048797492c7b3595a9a26200eff7223cc6e4b98
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-05-17 12:20:33 +0200

    Interface for possibleMoves, passing possibleMoves to Tile, initial declaration of handleKeyPress

--- src/components/Checkerboard/Checkerboard.tsx
+++ src/components/Checkerboard/Checkerboard.tsx
@@ -1,5 +1,6 @@
 import "./Checkerboard.css";
 import Tile from "../Tile/Tile";
+import { useState } from "react";
 
 // prettier-ignore
 const horizontalAxis: Array<string> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",];
@@ -10,26 +11,59 @@ interface Piece {
   y: number;
 }
 
-const piece: Piece[] = [{ x: 4, y: 4 }];
+interface IPossibleMoves {
+  x: number;
+  y: number;
+}
+
+const defaultPosition: Piece[] = [{ x: 4, y: 4 }];
+const defaultMoves: IPossibleMoves[] = [];
 
 function Board() {
   let board = [];
+  const [possibleMoves, setPossibleMoves] = useState(defaultMoves);
+  const [piecePosition, setPiecePosition] = useState(defaultPosition);
+
+  const handleKeyPress = (event: { key: string }) => {
+    if (possibleMoves.length > 0) {
+    } else if (possibleMoves.length === 0) {
+    }
+    if (event.key === "ArrowUp") {
+    }
+  };
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
       const number = j + i + 2;
       let isTherePiece: boolean = false;
+      let ifPossibleMove: boolean = false;
 
-      piece.forEach((p) => {
+      piecePosition.forEach((p) => {
         if (p.x === i && p.y === j) {
           isTherePiece = true;
         }
       });
 
-      board.push(<Tile number={number} isTherePiece={isTherePiece} />);
+      possibleMoves.forEach((m) => {
+        if (m.x === i && m.y === j) {
+          ifPossibleMove = true;
+        }
+      });
+
+      board.push(
+        <Tile
+          number={number}
+          isTherePiece={isTherePiece}
+          ifPossibleMove={ifPossibleMove}
+        />
+      );
     }
   }
-  return <div id="checkerboard">{board}</div>;
+  return (
+    <div id="checkerboard" onKeyPress={handleKeyPress}>
+      {board}
+    </div>
+  );
 }
 
 export default Board;
