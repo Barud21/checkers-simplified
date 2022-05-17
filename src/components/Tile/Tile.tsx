ccfcfeb948e0dc01a220f3f36e8eab5aadd3828a
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-05-17 12:17:27 +0200

    New property added to determine if the tile is a possible move

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,12 +1,21 @@
 import classNames from "classnames";
 import "./Tile.css";
 
-function Tile(props: { number: number; isTherePiece: boolean }) {
+function Tile(props: {
+  number: number;
+  isTherePiece: boolean;
+  ifPossibleMove: boolean;
+}) {
   if (props.number % 2 !== 0) {
     return <div className={"tile white-tile"}></div>;
   } else {
     return (
-      <div className={classNames({ piece: props.isTherePiece === true })}></div>
+      <div
+        className={classNames({
+          piece: props.isTherePiece === true,
+          move: props.ifPossibleMove === true,
+        })}
+      ></div>
     );
   }
 }
